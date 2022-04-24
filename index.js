cecb6063ff3f14de414175af718e6e89b9595fe7
Mark Poepping <mark.poepping@admiralcloud.com>
2022-04-24 21:39:22 +0200

    style(Misc): Formatted file | MP
    Formatted file
    Related issues:
    - https://github.com//issues/undefined

--- index.js
+++ index.js
@@ -464,15 +464,15 @@ const acaee = () => {
     /**
    * Return entry for sanitizer for a given field (and controller/action)
    */
-     const fieldDefinition = (config, field, controller, action) => {
-      const def = _.get(config.apiDoc, controller)
-      if (!def) return { message: 'noAPIdoc', additionalInfo: { controller, action } }
-      let fields = _.cloneDeep(_.get(def, 'fields'))
-      let fieldToUse = _.find(fields, f => {
-        if (_.indexOf(_.get(f, 'actions'), action) > -1 && _.get(f, 'field') === field) return f
-      })
-      return mapFieldDefinition(fieldToUse, action)
-    }
+  const fieldDefinition = (config, field, controller, action) => {
+    const def = _.get(config.apiDoc, controller)
+    if (!def) return { message: 'noAPIdoc', additionalInfo: { controller, action } }
+    let fields = _.cloneDeep(_.get(def, 'fields'))
+    let fieldToUse = _.find(fields, f => {
+      if (_.indexOf(_.get(f, 'actions'), action) > -1 && _.get(f, 'field') === field) return f
+    })
+    return mapFieldDefinition(fieldToUse, action)
+  }
 
   return {
     defaultValues,
